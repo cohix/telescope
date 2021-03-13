08e74b6c1e9484cd0ccf8b176d63d6fd1d6f99cb
cohix <connorjhicks@gmail.com>
2021-03-13 12:41:30 -0500

    update

---
+++ serve-file/target/wasm32-wasi/release/deps/suborbital-c73194f4a4854e44.d
@@ -0,0 +1,7 @@
+/Users/cohix-16/Workspaces/suborbital/telescope/serve-file/target/wasm32-wasi/release/deps/suborbital-c73194f4a4854e44.wasm: /Users/cohix-16/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.8.2/src/lib.rs
+
+/Users/cohix-16/Workspaces/suborbital/telescope/serve-file/target/wasm32-wasi/release/deps/libsuborbital-c73194f4a4854e44.rlib: /Users/cohix-16/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.8.2/src/lib.rs
+
+/Users/cohix-16/Workspaces/suborbital/telescope/serve-file/target/wasm32-wasi/release/deps/suborbital-c73194f4a4854e44.d: /Users/cohix-16/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.8.2/src/lib.rs
+
+/Users/cohix-16/.cargo/registry/src/github.com-1ecc6299db9ec823/suborbital-0.8.2/src/lib.rs:
